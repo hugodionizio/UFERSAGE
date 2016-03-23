0451a55b7f8928ba6b72051ecdc509c1c10df6c7
Hugo Dionizio Santos <dnhutos@hotmail.com>
2016-03-23 12:33:43 -0300

    Pré-adição de arquivos remanescentes e referenciados pelo OpenGL Programming Guide 7e

--- src/Examples/vortex.cpp
+++ src/Examples/vortex.cpp
@@ -15,7 +15,7 @@ float xrot, yrot, zrot;
 GLuint texture;
 GLuint pbo;
 
-unsigned char *image;
+unsigned char *imageVortex;
 int t = 0;
 int a = 200;
 int bVortex = 240;
@@ -89,16 +89,16 @@ int mainVortex(int argc, char **argv) {
 
 //create the data to put into the texture
 	int size = imageWidth * imageHeight * 4;
-	image = (unsigned char *) malloc(sizeof(unsigned char) * size);
+	imageVortex = (unsigned char *) malloc(sizeof(unsigned char) * size);
 	int i;
 	for (i = 0; i < size; i = i + 1)
-		image[i] = (i / 256);  //create the texture
+		imageVortex[i] = (i / 256);  //create the texture
 	glGenTextures((GLuint) 1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageWidth, imageHeight, 0, GL_RGBA,
-			GL_UNSIGNED_BYTE, image); //initialize the extentions needed for using a PBO
+			GL_UNSIGNED_BYTE, imageVortex); //initialize the extentions needed for using a PBO
 	glewInit();
 	if (!glewIsSupported("GL_VERSION_2_0 " "GL_ARB_pixel_buffer_object"))
 		printf("ERROR: Support for necessary OpenGL extensions missing."); //create the PBO
@@ -107,7 +107,7 @@ int mainVortex(int argc, char **argv) {
 	glPixelStorei(GL_PACK_ALIGNMENT, 1); //upload our host image data to the pixel buffer
 	glBindBuffer(GL_PIXEL_UNPACK_BUFFER_ARB, pbo);
 	glBufferData(GL_PIXEL_UNPACK_BUFFER_ARB,
-			sizeof(unsigned char) * imageWidth * imageHeight * 4, image,
+			sizeof(unsigned char) * imageWidth * imageHeight * 4, imageVortex,
 			GL_STREAM_DRAW);
 	glBindBuffer(GL_PIXEL_UNPACK_BUFFER_ARB, 0); //initialize the GL projection
 	glMatrixMode(GL_PROJECTION);
